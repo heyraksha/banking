56d9848af37b25c16100286584fb006a91778fca
heyraksha <er.rakshapaliwal@gmail.com>
2024-07-16 22:30:56 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body>
+        <main>
+          {children}
+        </main>
+        </body>
+      </html>
+    );
+  }
+  
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body> 
+        <main>
+          SIDEBAR
+          {children}
+        </main>
+        </body>
+      </html>
+    );
+  }
+  
